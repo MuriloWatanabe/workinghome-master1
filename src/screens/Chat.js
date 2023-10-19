b6f34b5d0b31dbadd945b9d5caba1ff28d14885a
Dominic-Krelling <dominic.krelling1@gmail.com>
2023-10-19 08:04:49 -0300

    chat

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,92 +1,82 @@
-import React from 'react';
-import {  TextInput, StyleSheet, View, Image, TouchableOpacity, } from 'react-native';
-import {  Card } from 'react-native-paper';
+import React from "react";
+import {
+  TextInput,
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { Card } from "react-native-paper";
 
-import Enviar from '../../icons/enviar-mensagem.png'
-import Icone from '../../icons/imagem-do-usuario-com-fundo-preto.png'
+import Enviar from "../../icons/enviar-mensagem.png";
+import Icone from "../../icons/imagem-do-usuario-com-fundo-preto.png";
 
-
-export default function Chat ({ navigation }) {
+export default function Chat({ navigation }) {
   return (
     <View style={styles.container}>
       <Card style={styles.card}>
-      <TouchableOpacity onPress={() => navigation.navigate("Home")} style={styles.icone}>
-           <Image source={Icone} style={styles.icone}></Image>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Home")}
+          style={styles.icone}
+        >
+          <Image source={Icone} style={styles.icone}></Image>
         </TouchableOpacity>
-            </Card>
-          <View >
-         <View style={styles.barmen}>
-            <TextInput type="email" name="fale" placeholder="Enviar mensagem" style={styles.text}/>      
-          <View>
-         <TouchableOpacity onPress={() => navigation.navigate("Chat")} style={styles.botao}>
-            <Image source={Enviar} style={{height: 30, width:30}}></Image>
+      </Card>
+      <View>
+        <View style={styles.barmen}>
+          <View style={styles.text}>
+            <TextInput
+              placeholder="Mensagem"
+              type="text"
+              name="fale"
+              style={{ width: 300, height: 30, backgroundColor: "grey", borderRadius:25, color: "white" }}
+            ></TextInput>
+          </View>
+          <TouchableOpacity>
+            <View style={styles.botao}>
+              <Image source={Enviar} style={{ height: 30, width: 30 }}></Image>
+            </View>
           </TouchableOpacity>
-       </View>
-     </View>
+        </View>
+      </View>
     </View>
-    <View>
-      <TextInput></TextInput>
-    </View>
-  </View>
   );
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     paddingTop: 10,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
 
-icone:{
-  width: 60,
-  height:60,
-  marginLeft: 5,
-},
+  icone: {
+    width: 60,
+    height: 60,
+    marginLeft: 5,
+  },
 
   input: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 12,
     margin: 10,
-    width: '100%',
-  },
-
-  botao: {
-    width: 50,
-    height: 50,
-    backgroundColor:"#CEC8C8",
-    margin: 60,
-    marginLeft:10,
-    borderRadius:25,
-    alignItems: "center",
-    justifyContent:"center"
+    width: "100%",
   },
 
   barmen: {
-  backgroundColor: 'white',
-  justifyContent: "space-evenly",
-  flexDirection: "row",
-  height: 45,
-  alignItems: "center",
-  marginTop:640,
-},
-
-  text:{ 
-    backgroundColor: '#CEC8C8',
-    borderRadius: 10,
-    margin: 10,
-    width: 200,
-    height: 30,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 640,
+    justifyContent: "center",
   },
 
-  card:{ 
+  card: {
     backgroundColor: "#3B8AEB",
-    width: '100%',
-    height: '10%',
+    width: "100%",
+    height: "10%",
     marginTop: -10,
     marginBottom: 23,
     borderRadius: -1,
   },
-
 });
